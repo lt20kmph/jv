ae3f19b3c1ae545fb4f8fcddbe44ee3fd660a037
oli <o.g.sargent@gmail.com>
2025-06-26 08:50:44 +0100

    feat: Redirect root route to /galleries

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -5,17 +5,12 @@ use crate::errors;
 use crate::models::models;
 use crate::tera_utils;
 use rocket::get;
-use rocket::response::content;
+use rocket::response::Redirect;
 
 #[get("/")]
 pub async fn get(
     db: &Db,
     _session: models::Session,
-) -> Result<content::RawHtml<String>, errors::AppError> {
-    let mut context = tera::Context::new();
-    let images = queries::get_gallery_images(db, 1).await;
-    context.insert("images", &images);
-    context.insert("gallery_id", &1);
-    let index = tera_utils::render_template_with_logging("index.html", &context)?;
-    Ok(content::RawHtml(index))
+) -> Redirect {
+    Redirect::to(rocket::uri!("/galleries"))
 }
